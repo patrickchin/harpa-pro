8852518b566bd2380a5b0062903eb94f0fe07435
Patrick Chin <patrick@harpa.pro>
2026-05-30 19:18:15 +0800

    Revert "fix(files): fetch image metadata even when uri is pre-resolved"
    This reverts commit 25af3c464ca2ff00c57c00531980f53bf4f8470b.

--- apps/mobile/components/files/ImagePreviewModal.tsx
+++ apps/mobile/components/files/ImagePreviewModal.tsx
@@ -378,7 +378,7 @@ function ImagePreviewBody({
   onInfo: (index: number, info: PhotoInfo) => void;
 }) {
   const { data } = useFileSignedUrl(fileId, {
-    enabled: Boolean(fileId),
+    enabled: !uri && Boolean(fileId),
   });
   const { data: thumbnailData } = useFileSignedUrl(thumbnailFileId, {
     enabled: !uri && Boolean(thumbnailFileId),
